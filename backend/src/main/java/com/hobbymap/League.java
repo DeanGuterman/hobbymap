29403f11fe60050c4bb706e3b7a5f248835d8507
DeanGuterman <deangut@gmail.com>
2025-05-16 18:24:51 +0300

    feat(backend): implement League entity using Lombok

---
+++ backend/src/main/java/com/hobbymap/League.java
@@ -0,0 +1,40 @@
+package com.hobbymap;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class League {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+
+    private String name;
+    private String city;
+
+    private String format;
+    private String weekday;
+
+    private String personOfContact;
+    private String personOfContactPhone;
+
+    private String siteLink;
+    private String whatsappLink;
+    private String facebookLink;
+    private String instagramLink;
+
+    private String notes;
+
+}
