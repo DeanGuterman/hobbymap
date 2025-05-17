9a3098eac0f5180a8ace0c5e51da105f0b7dce06
DeanGuterman <deangut@gmail.com>
2025-05-17 13:32:56 +0300

    fix: modify database comparison so it correctly compares to relevant columns, instead of defaulting to 'city'

--- backend/src/main/java/com/hobbymap/LeagueSpecifications.java
+++ backend/src/main/java/com/hobbymap/LeagueSpecifications.java
@@ -17,21 +17,21 @@ public class LeagueSpecifications {
     public static Specification<League> filterByFormat(String format){
         return (root, query, cb) -> {
             if (format == null) return null;
-            return cb.equal(root.get("city"), format);
+            return cb.equal(root.get("format"), format);
         };
     }
 
     public static Specification<League> filterByName(String name){
         return (root, query, cb) -> {
             if (name == null) return null;
-            return cb.equal(root.get("city"), name);
+            return cb.equal(root.get("name"), name);
         };
     }
 
     public static Specification<League> filterByWeekday(String weekday){
         return (root, query, cb) -> {
             if (weekday == null) return null;
-            return cb.equal(root.get("city"), weekday);
+            return cb.equal(root.get("weekday"), weekday);
         };
     }
 }
