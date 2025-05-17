696582e4e58d9b4e8e3356193c344745be479a84
DeanGuterman <deangut@gmail.com>
2025-05-17 12:48:20 +0300

    feat: add findAll method for JpaSpecification (mini)refactor

--- backend/src/main/java/com/hobbymap/LeagueRepository.java
+++ backend/src/main/java/com/hobbymap/LeagueRepository.java
@@ -1,5 +1,6 @@
 package com.hobbymap;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface LeagueRepository extends JpaRepository<League, Long> {
     List<League> findByCity(String city);
     List<League> findByFormat(String format);
     List<League> findByWeekday(String weekday);
+
+    List<League> findAll(Specification<League> spec);
 }
