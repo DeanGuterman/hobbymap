ea8f51fe51a2e0fa25c1aa22c5013e62abef5ca1
DeanGuterman <deangut@gmail.com>
2025-05-17 12:35:24 +0300

    feat: implement all 'getLeagueBy' methods in LeagueService

--- backend/src/main/java/com/hobbymap/LeagueService.java
+++ backend/src/main/java/com/hobbymap/LeagueService.java
@@ -15,4 +15,21 @@ public class LeagueService {
     public List<League> getAllLeagues(){
         return leagueRepository.findAll();
     }
+
+    public List<League> getAllLeaguesByCity(String city){
+        return leagueRepository.findByCity(city);
+    }
+
+    public List<League> getAllLeaguesByFormat(String format){
+        return leagueRepository.findByFormat(format);
+    }
+
+    public List<League> getAllLeaguesByName(String name){
+        return leagueRepository.findByName(name);
+    }
+
+    public List<League> getAllLeaguesByWeekday(String weekday){
+        return leagueRepository.findByWeekday(weekday);
+    }
+
 }
