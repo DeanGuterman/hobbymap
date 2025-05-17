29fd0207c21b0c8cbcaa40b215467620c35ccddf
DeanGuterman <deangut@gmail.com>
2025-05-17 12:44:44 +0300

    feat: start refactor for JpaSpecificationExecutor

--- backend/src/main/java/com/hobbymap/LeagueController.java
+++ backend/src/main/java/com/hobbymap/LeagueController.java
@@ -2,6 +2,8 @@ package com.hobbymap;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +19,12 @@ public class LeagueController {
     }
 
     @GetMapping("/leagues")
-    public ResponseEntity<List<League>> getAllLeagues(){
+    public ResponseEntity<List<League>> getLeagues(
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String format,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String weekday
+    ){
         List<League> leagues = leagueService.getAllLeagues();
         return ResponseEntity.ok(leagues);
     }
