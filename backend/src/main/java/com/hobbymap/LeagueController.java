4c5d9f447e0ce8b3a157f6cb838108b00e18bc9b
DeanGuterman <deangut@gmail.com>
2025-05-17 12:26:51 +0300

    feat: implement getAllLeagues endpoint

--- backend/src/main/java/com/hobbymap/LeagueController.java
+++ backend/src/main/java/com/hobbymap/LeagueController.java
@@ -9,13 +9,16 @@ import java.util.List;
 @RestController
 public class LeagueController {
     private final LeagueRepository leagueRepository;
+    private final LeagueService leagueService;
 
-    public LeagueController(LeagueRepository leagueRepository){
+    public LeagueController(LeagueRepository leagueRepository, LeagueService leagueService){
         this.leagueRepository = leagueRepository;
+        this.leagueService = leagueService;
     }
 
     @GetMapping("/leagues")
     public ResponseEntity<List<League>> getAllLeagues(){
-
+        List<League> leagues = leagueService.getAllLeagues();
+        return ResponseEntity.ok(leagues);
     }
 }
