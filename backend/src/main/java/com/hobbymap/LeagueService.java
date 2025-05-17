35cf8da928dc3285c931455c463e1dbd3a6d96c7
DeanGuterman <deangut@gmail.com>
2025-05-17 13:40:49 +0300

    feat: add method for filtering leagues by optional criteria

--- backend/src/main/java/com/hobbymap/LeagueService.java
+++ backend/src/main/java/com/hobbymap/LeagueService.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.hobbymap.LeagueSpecifications.*;
+
 @Service
 public class LeagueService {
     private final LeagueRepository leagueRepository;
 
     public LeagueService(LeagueRepository leagueRepository){
         this.leagueRepository = leagueRepository;
+    }
+
+    public List<League> getLeagues(String city, String format, String name, String weekday){
+        return leagueRepository.findAll(filterByCity(city)
+                .and(filterByFormat(format))
+                .and(filterByName(name))
+                .and(filterByWeekday(weekday))
+        );
     }
 
     public List<League> getAllLeagues(){
