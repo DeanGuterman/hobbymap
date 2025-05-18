9912eb2f85c61685c4baf570232912c955510861
DeanGuterman <deangut@gmail.com>
2025-05-18 17:51:48 +0300

    feat: add initial database seeder on startup (not functional yet)

---
+++ backend/src/main/java/com/hobbymap/config/DataSeeder.java
@@ -0,0 +1,12 @@
+package com.hobbymap.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
+@RequiredArgsConstructor
+public class DataSeeder implements CommandLineRunner {
+}
