157905dc1633370721c3ff17ecf3968052f6a87d
DeanGuterman <deangut@gmail.com>
2025-05-18 14:18:54 +0300

    feat: add LeagueMapper as a helper function to convert League objects to LeagueDTO

--- backend/src/main/java/com/hobbymap/LeagueMapper.java
+++ backend/src/main/java/com/hobbymap/LeagueMapper.java
@@ -1,4 +1,12 @@
 package com.hobbymap;
 
 public class LeagueMapper {
+    public static LeagueDTO toDto(League league){
+        return LeagueDTO.builder()
+                .city(league.getCity())
+                .format(league.getFormat())
+                .name(league.getName())
+                .weekday(league.getWeekday())
+                .build();
+    }
 }
