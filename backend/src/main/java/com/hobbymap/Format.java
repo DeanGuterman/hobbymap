19659bf17c6f01e3a278d6de25c07eff7bcc96ad
DeanGuterman <deangut@gmail.com>
2025-05-18 14:52:21 +0300

    feat: add Weekday and Format as enums

---
+++ backend/src/main/java/com/hobbymap/Format.java
@@ -0,0 +1,5 @@
+package com.hobbymap;
+
+public enum Format {
+    LIMITED, STANDARD, PIONEER, MODERN, LEGACY, PAUPER, COMMANDER
+}
---
+++ backend/src/main/java/com/hobbymap/Weekday.java
@@ -0,0 +1,5 @@
+package com.hobbymap;
+
+public enum Weekday {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
